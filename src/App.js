99ee641c054d7944fa4093eab53ac7c9f4cf3210
Quang Tran <tranvietquang3077@gmail.com>
2020-09-08 02:50:33 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import socket from "./socketIO";
+import { Button, ProgressBar, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [yesCount, setYesCount] = React.useState(0);
+  const [sosoCount, setSosoCount] = React.useState(0);
+  const [noCount, setNoCount] = React.useState(0);
+  const [total, setTotal] = React.useState(0);
+
+  const sendVote = (option) => {
+    socket.emit("vote", option);
+  };
+
+  React.useEffect(() => {
+    socket.on("connection");
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  
+
+  React.useEffect(() => {
+    socket.on("receiveVote", (option) => {
+      setTotal(total + 1);
+      if (option === "yes") {
+        setYesCount(yesCount + 1);
+      }
+      if (option === "soso") {
+        setSosoCount(sosoCount + 1);
+      }
+      if (option === "no") {
+        setNoCount(noCount + 1);
+      }
+    });
+  }, [total, noCount, yesCount, sosoCount]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>Do you understand today's lecture ?</h1>
+        <Button variant="success" onClick={(e) => sendVote("yes")}>
+          Yes
+        </Button>
+        <Button variant="warning" onClick={(e) => sendVote("soso")}>
+          So So
+        </Button>
+        <Button variant="danger" onClick={(e) => sendVote("no")}>
+          Not at all
+        </Button>
+
+        <ProgressBar>
+          <ProgressBar
+            striped
+            variant="success"
+            now={(yesCount * 100) / total}
+            key={1}
+          />
+          <ProgressBar
+            striped
+            variant="warning"
+            now={(sosoCount * 100) / total}
+            key={2}
+          />
+          <ProgressBar
+            striped
+            variant="danger"
+            now={(noCount * 100) / total}
+            key={3}
+          />
+        </ProgressBar>
+      </Container>
     </div>
   );
 }
